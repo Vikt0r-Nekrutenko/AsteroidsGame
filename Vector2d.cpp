3f24fddd73bf999824d640d12f64c472131ba182
Vikt0r <vikt0r.nekrut@outlook.com>
2020-01-04 12:55:06 +0200

    vector added

---
+++ Vector2d.cpp
@@ -0,0 +1,47 @@
+#include "Vector2d.h"
+#include <cmath>
+
+Vector2d::Vector2d(float x, float y) :
+    m_x{x}, m_y{y}
+{
+
+}
+
+Vector2d Vector2d::operator +(const Vector2d &vec) const
+{
+    return { m_x + vec.m_x, m_y + vec.m_y };
+}
+
+Vector2d Vector2d::operator -(const Vector2d &vec) const
+{
+    return { m_x - vec.m_x, m_y - vec.m_y };
+}
+
+Vector2d Vector2d::operator *(const float s) const
+{
+    return { m_x * s, m_y * s };
+}
+
+Vector2d Vector2d::operator /(const float s) const
+{
+    return { m_x / s, m_y / s };
+}
+
+Vector2d &Vector2d::operator +=(const Vector2d &vec)
+{
+    m_x += vec.m_x;
+    m_y += vec.m_y;
+    return *this;
+}
+
+Vector2d &Vector2d::operator -=(const Vector2d &vec)
+{
+    m_x -= vec.m_x;
+    m_y -= vec.m_y;
+    return *this;
+}
+
+float Vector2d::distanceTo(const Vector2d &vec) const
+{
+    return sqrtf(powf(m_x - vec.m_x, 2.f) + powf(m_y - vec.m_y, 2.f));
+}
---
+++ Vector2d.h
@@ -0,0 +1,21 @@
+#ifndef VECTOR2D_H
+#define VECTOR2D_H
+
+
+struct Vector2d
+{
+    float m_x, m_y;
+
+    Vector2d(float x = 0.f, float y = 0.f);
+    Vector2d operator + (const Vector2d &vec) const;
+    Vector2d operator - (const Vector2d &vec) const;
+    Vector2d operator * (const float s) const;
+    Vector2d operator / (const float s) const;
+
+    Vector2d &operator += (const Vector2d &vec);
+    Vector2d &operator -= (const Vector2d &vec);
+
+    float distanceTo(const Vector2d &vec) const;
+};
+
+#endif // VECTOR2D_H
